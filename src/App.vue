42f835d10367c4897da5c0d3d1ec7c0f0dfd737c
Andrew Mondor <mondor.andrew@gmail.com>
2021-12-22 01:12:54 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+     <HelloWorld hello="👋🏽 HELLO MY NAME IS ANDREW"/>
+     <HelloWorld button="https://www.malt.fr/profile/andrewmondor"/>
+    <HelloWorld msg="I’m a Freelance Software Engineer from Guadeloupe, France. 🇫🇷"/>
+    <HelloWorld paragraph="I craft high-performing, beautiful websites that are conversion-focused, brand-accurate & people-friendly." />
+  </div>
+</template>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Futura, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 200px;
+  margin-left: 150px;
+  width: 600px;
+  font-size: 22px;
+
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+         <p>{{ hello }}</p>
+    <h1>{{ msg }}</h1>
+    <p>{{paragraph}}</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    hello: String,
+    paragraph:String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 20px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+</style>
